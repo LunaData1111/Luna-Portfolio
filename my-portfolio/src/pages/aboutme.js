2356a7c68cf1d59508d08bd000a8653a5c4558a1
Christian Luna <christianlunaduenas@gmail.com>
2022-11-26 14:49:40 -0800

    Aboutme and contact is all finished.

--- my-portfolio/src/pages/aboutme.js
+++ my-portfolio/src/pages/aboutme.js
@@ -17,7 +17,7 @@ function AboutMePage() {
       <VStack
         spacing={7}
         paddingTop={5}
-        backgroundImage="https://images.unsplash.com/photo-1436491865332-7a61a109cc05?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2374&q=80"
+        backgroundImage="https://images.unsplash.com/photo-1574027542183-77efe00ca49f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1998&q=80"
         backgroundPosition="center"
         backgroundRepeat="no-repeat"
       >
@@ -28,17 +28,21 @@ function AboutMePage() {
           <CardBody>
             <Stack mt="6" spacing="3">
               <Image
-                src="https://lh3.googleusercontent.com/pw/AL9nZEWWEGUXOgRZnC5ao0lHIPbnWrsA82JSSCo_reewkrU2_lBMMFU2ohv7lQKD5AEvv_qZYmwvQRMnoKwaOJQekkxjWU8dTVLcSnBHzuZG2XebHCkQ10OPMN50mu5mWPTIuyZFRywB_DT5kbnb8GezpOBz-bP_xd98-5xWUgpNupKgQEszZk9v6x8RoVXlidez0bmp2Ec0UujzKkZjOU-oxZkpnagUpUpneUdgrM-8hyLTWWsvX3WFhRRpfShLKiOWJ6lV8qPQstklgt9lSoX7OwgG-wAnF1kbq2IOKCJ40y63alPKMYK7rQbQ9s5SaMatm3yd7d7A8gv3seIDi-TtoI_sAI1UzmDQyYL0ubFKebE5nwe4YdMkE63IAxClWCq30-_dzoZk-vAMQcFcScnK5NGKQvqnLsizFzTUAy5Ac0pNnTU4tCa8GONdTazFe9ANJWsCew98JrmA-zQKoW0fy_0AO0wXWcI3UoRe861PcxsAAYMWbt8dC9F6eas5Y1LhCPbcT2QStT0jnlU27uxrKFVEhLweo8i7-tk8Dx0FcvxhzCP4h1eYY27NY5ipmZCKO1xMjVGXT8a_rKPqgqQpbqj9Q09HuL3IRHP1kOIGS5m8e-IO03SQOl-TbwsjEqIxlfeDx_16kosGkWFOYptxtSjPZ87ivR7sLUBwqzE2u7G02ruSz42kqLaVNVT-_0x4np78sN7Ypva4iVclVPWXQpCIulHzlZkMk9fvOkpdnw9HcnnjU_XodtNV5tbHKw4nGbv5F8mJ2V9psHSyWkhsZOaUSFpCzWevdn6qPjI6dP5nM33jn2bxkKHoPhtL67e8oMRhLA5zbc8Hpblb1WIyQlpeg1fSaK6znN5zkZFQlmrfQ_wJMPYcOn-Z-sHX52aSrhLCY7lBmFLTar2V8BlNalPys4FGd40ue3f5aXPjERPEOHBgjDHsHIk0QpyLIKgxaHqCrqqEbU1TPQzJh-WiO5-cirv6RCYi5i47rNtUHRx4zLb56q9tSU0sjkgzXgbh5c2elVPVKK3LPW8v-DqNmZg8N4zLlQvWQqnL_Nz6EbAkPJu4Xz_VLr3rZfm9m3Q_HMRn8QsE=w1007-h1162-no?authuser=0"
+                src="https://i.imgur.com/pnNoPjG.jpg"
                 alt="Photo of Christian LunaDuenas"
                 borderRadius="3xl"
               />
               <Heading size="md">Christian LunaDuenas Portfolio</Heading>
               <Text>
                 I am a graduate student from the University of Utah Full Stack
-                Web Development Bootcamp. I'm always learning and willing to adapt under pressure. 
-                I'am looking forward to a fast work enivorment. Solving puzzles and seeing them come to life is exciting.
-                I would like to work in back end development. I have intrest to further my edcation in softerware development and quantum software.
-                Over all I am thrilled to work in the tech world. Striving to improve software developing skills is my number one goal, so I may make a difference one day. 
+                Web Development Bootcamp. I'm always learning and willing to
+                adapt under pressure. I'am looking forward to a fast work
+                enivorment. Solving puzzles and seeing them come to life is
+                exciting. I would like to work in back end development. I have
+                intrest to further my edcation in softerware development and
+                quantum software. Over all I am thrilled to work in the tech
+                world. Striving to improve software developing skills is my
+                number one goal, so I may make a difference one day.
               </Text>
             </Stack>
           </CardBody>
--- my-portfolio/src/pages/contact.js
+++ my-portfolio/src/pages/contact.js
@@ -1,88 +1,76 @@
 import {
-    Box,
-    Button,
-    FormControl,
-    FormLabel,
-    Heading,
-    IconButton,
-    Input,
-    InputGroup,
-    InputLeftElement,
-    Link,
-    Stack,
-    Textarea,
-    Tooltip,
-    useClipboard,
-    useColorModeValue,
-    VStack,
-    ChakraProvider
-  } from '@chakra-ui/react';
-  import React from 'react';
-  import { FaGithub, FaLinkedin, FaStackOverflow } from "react-icons/fa";
-  import { MdEmail, MdOutlineEmail } from 'react-icons/md';
-  
-  export default function ContactFormWithSocialButtons() {
-    const { hasCopied, onCopy } = useClipboard('example@example.com');
-  
-    return (
-      <ChakraProvider>
-            <VStack spacing={7} paddingTop={5}
-              backgroundImage="https://images.unsplash.com/photo-1504609773096-104ff2c73ba4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80"
-              backgroundPosition="center"
-              backgroundRepeat="no-repeat">
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Heading,
+  IconButton,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Link,
+  Stack,
+  Textarea,
+  Tooltip,
+  useClipboard,
+  useColorModeValue,
+  VStack,
+  ChakraProvider,
+} from "@chakra-ui/react";
+import React from "react";
+import { FaGithub, FaLinkedin, FaStackOverflow } from "react-icons/fa";
+import { MdEmail, MdOutlineEmail } from "react-icons/md";
+import { BsPerson } from "react-icons/bs";
+
+export default function ContactFormWithSocialButtons() {
+  const { hasCopied, onCopy } = useClipboard("example@example.com");
+
+  return (
+    <ChakraProvider>
+      <VStack
+        spacing={7}
+        paddingTop={5}
+        backgroundImage="https://images.unsplash.com/photo-1574027542338-98e75acfd385?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80"
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+      >
         <Box
           borderRadius="lg"
           m={{ base: 5, md: 16, lg: 10 }}
           p={{ base: 5, lg: 16 }}
-          
-          >
+        >
           <Box>
             <VStack spacing={{ base: 4, md: 8, lg: 20 }}>
               <Heading
                 fontSize={{
-                  base: '4xl',
-                  md: '5xl',
+                  base: "4xl",
+                  md: "5xl",
                 }}
-                color='white'>
+                color="black"
+              >
                 Get in Touch
               </Heading>
-  
+
               <Stack
                 spacing={{ base: 4, md: 8, lg: 20 }}
-                direction={{ base: 'column', md: 'row' }}>
+                direction={{ base: "column", md: "row" }}
+              >
                 <Stack
                   align="center"
                   justify="space-around"
-                  direction={{ base: 'row', md: 'column' }}>
-                  <Tooltip
-                    label={hasCopied ? 'Email Copied!' : 'Copy Email'}
-                    closeOnClick={false}
-                    hasArrow>
-                    <IconButton
-                      aria-label="email"
-                      variant="ghost"
-                      size="lg"
-                      fontSize="3xl"
-                      icon={<MdEmail />}
-                      _hover={{
-                        bg: 'blue.500',
-                        color: useColorModeValue('white', 'gray.700'),
-                      }}
-                      onClick={onCopy}
-                      isRound
-                    />
-                  </Tooltip>
-  
+                  direction={{ base: "row", md: "column" }}
+                >
+
                   <Link href="https://github.com/LunaData1111">
                     <IconButton
                       aria-label="github"
-                      variant="ghost"
+                    //   variant="ghost"
                       size="lg"
                       fontSize="3xl"
                       icon={<FaGithub />}
                       _hover={{
-                        bg: 'blue.500',
-                        color: useColorModeValue('white', 'gray.700'),
+                        bg: "red.500",
+                        color: useColorModeValue("white", "gray.50"),
                       }}
                       isRound
                     />
@@ -91,52 +79,57 @@ import {
                   <Link href="https://stackoverflow.com/users/19222238/lunadata1111">
                     <IconButton
                       aria-label="Stack Overflow"
-                      variant="ghost"
+                    //   variant="ghost"
                       size="lg"
                       fontSize="3xl"
                       icon={<FaStackOverflow />}
                       _hover={{
-                        bg: 'blue.500',
-                        color: useColorModeValue('white', 'gray.700'),
+                        bg: "red.500",
+                        color: useColorModeValue("white", "gray.700"),
                       }}
                       isRound
                     />
                   </Link>
-  
+
                   <Link href="https://www.linkedin.com/in/christian-luna-9263b4240/">
                     <IconButton
                       aria-label="linkedin"
-                      variant="ghost"
+                    //   variant="ghost"
                       size="lg"
                       icon={<FaLinkedin size="28px" />}
                       _hover={{
-                        bg: 'blue.500',
-                        color: useColorModeValue('white', 'gray.700'),
+                        bg: "red.500",
+                        color: useColorModeValue("white", "gray.700"),
                       }}
                       isRound
                     />
                   </Link>
                 </Stack>
-  
+
                 <Box
-                  bg={useColorModeValue('white', 'gray.700')}
+                  bg={useColorModeValue("white", "gray.700")}
                   borderRadius="lg"
                   p={8}
-                  color={useColorModeValue('gray.700', 'whiteAlpha.900')}
-                  shadow="base">
+                  color={useColorModeValue("gray.700", "whiteAlpha.900")}
+                  shadow="base"
+                >
                   <VStack spacing={5}>
                     <FormControl isRequired>
                       <FormLabel>Name</FormLabel>
-  
+
                       <InputGroup>
                         <InputLeftElement children={<BsPerson />} />
-                        <Input type="text" name="name" placeholder="Your Name" />
+                        <Input
+                          type="text"
+                          name="name"
+                          placeholder="Your Name"
+                        />
                       </InputGroup>
                     </FormControl>
-  
+
                     <FormControl isRequired>
                       <FormLabel>Email</FormLabel>
-  
+
                       <InputGroup>
                         <InputLeftElement children={<MdOutlineEmail />} />
                         <Input
@@ -146,10 +139,10 @@ import {
                         />
                       </InputGroup>
                     </FormControl>
-  
+
                     <FormControl isRequired>
                       <FormLabel>Message</FormLabel>
-  
+
                       <Textarea
                         name="message"
                         placeholder="Your Message"
@@ -157,15 +150,16 @@ import {
                         resize="none"
                       />
                     </FormControl>
-  
+
                     <Button
-                      colorScheme="blue"
-                      bg="blue.400"
+                      colorScheme="red"
+                      bg="red.400"
                       color="white"
                       _hover={{
-                        bg: 'blue.500',
+                        bg: "gray.900",
                       }}
-                      isFullWidth>
+                      isFullWidth
+                    >
                       Send Message
                     </Button>
                   </VStack>
@@ -174,7 +168,7 @@ import {
             </VStack>
           </Box>
         </Box>
-        </VStack>
-      </ChakraProvider>
-    );
-  }
+      </VStack>
+    </ChakraProvider>
+  );
+}
