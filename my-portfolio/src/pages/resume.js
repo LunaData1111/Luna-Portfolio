77267e42c23b81d05a4bf69fa1da1cdff7a52f50
Christian Luna <christianlunaduenas@gmail.com>
2022-11-27 10:09:02 -0800

    Resume page is now up and running.

--- my-portfolio/src/pages/resume.js
+++ my-portfolio/src/pages/resume.js
@@ -5,14 +5,14 @@ function ResumePage() {
   return (
     <ChakraProvider>
       <VStack spacing={7} paddingTop={5}>
-        <Heading size="xl">Christian's Resume</Heading>
+        <Heading size="xl">Christian Luna's Resume</Heading>
         <Image
           src="https://i.imgur.com/OqDNxuP.png"
           alt="Christian Luna's resume"
           borderRadius="lg"
         />
         <Link
-          href="https://docs.google.com/document/d/1cekdX0KYNistluzVeWg_OGJcO9HYIBJuwJYUW8oXByI/edit?usp=sharing"
+          href="https://docs.google.com/document/d/1p6QnqGnkuqSr_Zbsxnh-T8nTdSmQsK65-0vE6CauKX4/edit?usp=sharing"
           isExternal
         >
           Link <ExternalLinkIcon mx="2px" />
